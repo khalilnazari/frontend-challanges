ba97cf40824f6174a8714500b314116b043f56bd
khalilnazari <khalil.deve@gmail.com>
2022-04-22 16:28:30 +0800

    in interactive-vote-component, fixed a bug

--- interactive-rating-component/app.js
+++ interactive-rating-component/app.js
@@ -1,31 +1,14 @@
 
 const cardForm = document.querySelector('.card-form')
 const cardResult = document.querySelector('.card-result')
-const submitBtn = document.querySelector('.submit-btn')
 const voteForm = document.getElementById('vote-form')
 const showVote = document.getElementById('show-vote')
 
-console.log(voteForm)
-
-// function submitForm (e) {
-//     e.preventDefault(); 
-
-// }
+// when submitting the form.
 voteForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const vaote = document.querySelector('input[name="vote"]:checked').value;
-    showVote.innerText = vaote; 
+    const vote = document.querySelector('input[name="vote"]:checked').value;
+    showVote.innerText = vote; 
     cardForm.classList.add('hide'); 
     cardResult.classList.add('show'); 
-    // console.log(vaote)
 })
-
-// submitBtn.addEventListener('click', (e) => {
-//     e.preventDefault();
-
-//     // when submiting form
-//     cardForm.classList.add('hide'); 
-
-//     cardResult.classList.add('show'); 
-// })
-    
